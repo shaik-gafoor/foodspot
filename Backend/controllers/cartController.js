d3ed94fcd64197de9bdbe155eb3215beb523c24b
shaik-gafoor <gafoor7898@gmail.com>
2025-07-20 07:52:57 +0530

    Added functionality for removeFromCart in backend

--- Backend/controllers/cartController.js
+++ Backend/controllers/cartController.js
@@ -24,7 +24,16 @@ const addToCart = async (req, res) => {
 const removeFromCart = async (req, res) => {
   try {
     let userData = await userModel.findById(req.body.userId);
-  } catch {}
+    let cartData = await userData.cartData;
+    if (cartData[req.body.itemId] > 0) {
+      cartData[req.body.itemId] -= 1;
+    }
+    await userModel.findByIdAndUpdate(req.body.userId, { cartData });
+    res.json({ success: true, message: "Removed From Cart" });
+  } catch {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
 };
 
 //fetch!user cart data
