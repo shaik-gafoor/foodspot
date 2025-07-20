93ef6a2c7da85a0a542e4740633354bf53ead0f6
shaik-gafoor <gafoor7898@gmail.com>
2025-07-20 20:01:25 +0530

    added verify page

--- Frontend/src/pages/verify/verify.jsx
+++ Frontend/src/pages/verify/verify.jsx
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import "./verify.css";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { StoreContext } from "../../Context/StoreContext";
+import axios from "axios";
 
 const verify = () => {
   const [searchParams, setParams] = useSearchParams();
   const success = searchParams.get("success");
   const orderId = searchParams.get("orderId");
+  const { url } = useContext(StoreContext);
+  const navigate = useNavigate();
 
-  console.log(success, orderId);
+  const verifyPayment = async () => {
+    const response = await axios.post(url + "/api/order/verify", {
+      success,
+      orderId,
+    });
+    if (response.data.success) {
+      navigate("/myorders");
+    } else {
+      navigate("/");
+    }
+  };
 
-  return <div></div>;
+  useEffect(() => {
+    verifyPayment();
+  }, []);
+
+  return (
+    <div className="verify">
+      <div className="spinner"></div>
+    </div>
+  );
 };
 
 export default verify;
