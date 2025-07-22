5d1b9a00e0b4f7788ef6cb967a2ff49db156ce67
shaik-gafoor <gafoor7898@gmail.com>
2025-07-22 23:23:31 +0530

    created API for displaying all orders in backend

--- Backend/controllers/orderController.js
+++ Backend/controllers/orderController.js
@@ -117,4 +117,16 @@ const userOrders = async (req, res) => {
   }
 };
 
-export { placeOrder, verifyOrder, userOrders };
+//isting orders for  admin pannel
+
+const listOrders = async (req, res) => {
+  try {
+    const orders = await orderModel.find({});
+    res.json({ success: true, data: orders });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+export { placeOrder, verifyOrder, userOrders, listOrders };
