9e247e0ebaad4fe1062f88f528f0d212ca10ed18
Shaik Gafoor <gafoor7898@gmail.com>
2025-07-23 10:49:05 +0530

    modified API PORT

--- Frontend/src/Context/StoreContext.jsx
+++ Frontend/src/Context/StoreContext.jsx
@@ -7,7 +7,7 @@ export const StoreContext = createContext(null);
 
 export const StoreContextProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = import.meta.env.VITE_API_URL;
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
